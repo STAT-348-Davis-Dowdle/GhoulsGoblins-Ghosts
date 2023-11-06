d6243592145f5e08619b4de33ee21f3828c5300d
Davis Dowdle <davis.dowdle@gmail.com>
2023-11-06 13:48:33 -0700

    Neural Networks

--- GhoulsGoblinsGhosts.R
+++ GhoulsGoblinsGhosts.R
@@ -2,6 +2,7 @@ library(vroom)
 library(tidymodels)
 library(embed)
 library(kernlab)
+library(keras)
 
 # set working directory
 setwd("C:/Users/davis/OneDrive - Brigham Young University/Documents/skool/new/stat 348/GhoulsGoblins&Ghosts/GhoulsGoblinsGhosts")
@@ -62,3 +63,50 @@ predictions <- final_workflow %>%
 submission <- data.frame(id = test$id, type = predictions$.pred_class)
 
 write.csv(submission, "svm_submission.csv", row.names = F)
+
+
+# Neural Network
+nnrecipe <- recipe(type ~ ., data = train) %>%
+  update_role(id, new_role = "id") %>%
+  step_mutate_at(color, fn = factor) %>%
+  step_dummy(color) %>%
+  step_range(all_numeric_predictors(), min = 0, max = 1)
+
+nnmodel <- mlp(hidden_units = tune(),
+               epochs = 50) %>%
+  set_engine("nnet") %>%
+  set_mode("classification")
+
+nnwf <- workflow() %>%
+  add_recipe(nnrecipe) %>%
+  add_model(nnmodel)
+
+nntuning_grid <- grid_regular(hidden_units(range = c(1, 80)),
+                              levels = 30)
+
+nnfolds <- vfold_cv(train, v = 5)
+
+nntuned <- nnwf %>%
+  tune_grid(resamples = nnfolds, 
+            grid = nntuning_grid)
+
+nntuned %>%
+  collect_metrics() %>%
+  filter(.metric == "accuracy") %>%
+  ggplot(aes(x= hidden_units, y = mean)) + geom_line()
+
+#ggsave("nnplot.pdf")
+
+nnbest <- nntuned %>%
+  select_best("accuracy")
+
+nnfinalwf <- nnwf %>%
+  finalize_workflow(nnbest) %>%
+  fit(train)
+
+nnpredictions <- nnfinalwf %>%
+  predict(new_data = test, type = "class")
+
+nnsubmission <- data.frame(id = test$id, type = nnpredictions$.pred_class)
+
+write.csv(nnsubmission, "nnsubmission.csv", row.names = F)
