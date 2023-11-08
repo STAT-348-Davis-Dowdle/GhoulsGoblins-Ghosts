d697feff179863048c861ce7c9f6c899b4239a50
Davis Dowdle <davis.dowdle@gmail.com>
2023-11-08 13:43:44 -0700

    Boosting and Bart

--- GhoulsGoblinsGhosts.R
+++ GhoulsGoblinsGhosts.R
@@ -3,6 +3,9 @@ library(tidymodels)
 library(embed)
 library(kernlab)
 library(keras)
+library(bonsai)
+library(lightgbm)
+library(dbarts)
 
 # set working directory
 setwd("C:/Users/davis/OneDrive - Brigham Young University/Documents/skool/new/stat 348/GhoulsGoblins&Ghosts/GhoulsGoblinsGhosts")
@@ -110,3 +113,40 @@ nnpredictions <- nnfinalwf %>%
 nnsubmission <- data.frame(id = test$id, type = nnpredictions$.pred_class)
 
 write.csv(nnsubmission, "nnsubmission.csv", row.names = F)
+
+
+# Boosting
+boost_model <- boost_tree(tree_depth = tune(),
+                          trees = tune(),
+                          learn_rate = tune()) %>%
+  set_engine("lightgbm") %>%
+  set_mode("classification")
+
+boostwf <- workflow() %>%
+  add_recipe(recipe) %>%
+  add_model(boost_model)
+
+boost_grid <- grid_regular(tree_depth(),
+                           trees(),
+                           learn_rate(), 
+                           levels = 3)
+
+boost_folds <- vfold_cv(train, v = 5, repeats = 1)
+
+boost_results <- boostwf %>%
+  tune_grid(resamples = boost_folds,
+            grid = boost_grid)
+
+bestboost <- boost_results %>%
+  select_best("accuracy")
+
+boost_wf <- boostwf %>%
+  finalize_workflow(bestboost) %>%
+  fit(train)
+
+boost_predictions <- boost_wf %>%
+  predict(new_data = test, type = "class")
+
+boost_submission <- data.frame(id = test$id, type = boost_predictions$.pred_class)
+
+write.csv(boost_submission, "boost_submission.csv", row.names = F)
